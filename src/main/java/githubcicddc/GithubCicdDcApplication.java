12224247d78afb10573902ba9bd9a2e595d98bed
johnkhub <johnkatanga777@gmail.com>
2023-01-26 18:22:46 +0200

    first commit

---
+++ src/main/java/githubcicddc/GithubCicdDcApplication.java
@@ -0,0 +1,13 @@
+package githubcicddc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdDcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdDcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/githubcicddc/GithubCicdDcApplicationTests.java
@@ -0,0 +1,13 @@
+package githubcicddc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdDcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
